3c47381f06a34400e32909e81d22c9188d3d9db7
YukiMATSUOKA <yumitukai.yuki@gmail.com>
2021-02-19 01:58:32 +0900

    add InputNumDialog

--- app/src/main/java/com/example/drawmulticircle/DrawCircleView.java
+++ app/src/main/java/com/example/drawmulticircle/DrawCircleView.java
@@ -5,7 +5,10 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.RectF;
+import android.os.Handler;
+import android.os.Looper;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.View;
 
 import java.util.ArrayList;
@@ -18,12 +21,15 @@ public class DrawCircleView extends View {
     int rangeY = 300;
     RectF rect;
 
-    ArrayList<int[]> colorList;
-    ArrayList<Integer> values = new ArrayList<>();
-    ArrayList<Float> sweepAngles = new ArrayList<>();
-    ArrayList<Float> startAngles = new ArrayList<>();
+    private ArrayList<int[]> colorList;
+    private ArrayList<Integer> values = new ArrayList<>();
+    private ArrayList<Float> sweepAngles = new ArrayList<>();
+    private ArrayList<Float> startAngles = new ArrayList<>();
     float sumValue;
     float sweepAngle = -90;
+
+    private  Handler handler = new Handler(Looper.getMainLooper());
+    private  Runnable runnable;
 
     public DrawCircleView(Context context) {
         super(context);
@@ -34,29 +40,6 @@ public class DrawCircleView extends View {
         rect = new RectF();
 
         colorList = ColorList.getColorList();
-
-        values.add(1000);
-        values.add(750);
-        values.add(500);
-        values.add(250);
-        values.add(250);
-        values.add(125);
-        values.add(125);
-
-        for (int i = 0; i < values.size(); i++) {
-            sumValue += values.get(i);
-        }
-
-        for (int i = 0; i < values.size(); i++) {
-            sweepAngles.add((values.get(i) / sumValue) * 360);
-        }
-
-        for (int i = -1; i < values.size() - 1; i++) {
-            if (i != -1) {
-                sweepAngle += (double) sweepAngles.get(i);
-            }
-            startAngles.add(sweepAngle);
-        }
     }
 
     @Override
@@ -67,13 +50,27 @@ public class DrawCircleView extends View {
                 centerX + rangeX, centerY + rangeY);
 
         for (int i = 0; i < values.size(); i++) {
+            sumValue += values.get(i);
+        }
+        Log.d("test", "sumValue:"+sumValue);
+
+        for (int i = 0; i < values.size(); i++) {
+            sweepAngles.add((values.get(i) / sumValue) * 360);
+        }
+        Log.d("test", "sweepAngles:"+sweepAngles);
+
+        for (int i = -1; i < values.size() - 1; i++) {
+            if (i != -1) {
+                sweepAngle += (double) sweepAngles.get(i);
+            }
+            startAngles.add(sweepAngle);
+        }
+        Log.d("test", "startAngles:"+startAngles);
+
+        for (int i = 0; i < values.size(); i++) {
             canvas.drawArc(rect, startAngles.get(i), sweepAngles.get(i), false,
                     setPaint(colorList.get(i)));
         }
-    }
-
-    public void showCanvas() {
-        invalidate();
     }
 
     private Paint setPaint(int[] colorList) {
@@ -83,4 +80,20 @@ public class DrawCircleView extends View {
         paint.setStyle(Paint.Style.STROKE);
         return paint;
     }
+
+    public void addValue(int value){
+        values.add(value);
+        runnable = new Runnable() {
+            @Override
+            public void run() {
+                invalidate();
+                sumValue = 0;
+                sweepAngles.clear();
+                sweepAngle = -90;
+                startAngles.clear();
+            }
+        };
+        handler.post(runnable);
+    }
+
 }
--- app/src/main/java/com/example/drawmulticircle/MainActivity.java
+++ app/src/main/java/com/example/drawmulticircle/MainActivity.java
@@ -1,18 +1,88 @@
 package com.example.drawmulticircle;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.NumberPicker;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     DrawCircleView drawCircleView;
+    Button button;
+    LayoutInflater inflater;
+    View layout;
+    NumberPicker numPicker0, numPicker1, numPicker2, numPicker3,numPicker4,numPicker5;
+    int[] num = new int[6];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        button = findViewById(R.id.button);
+        button.setOnClickListener(this);
+
         drawCircleView = findViewById(R.id.drawCircleView);
-        drawCircleView.showCanvas();
+    }
+
+    @Override
+    public void onClick(View view) {
+        NumPickerDialog();
+    }
+
+    void NumPickerDialog(){
+        inflater = (LayoutInflater)this.getSystemService(LAYOUT_INFLATER_SERVICE);
+        layout = inflater.inflate(R.layout.dialog, (ViewGroup) findViewById(R.id.layout));
+
+        numPicker0 = layout.findViewById(R.id.numPicker0);
+        numPicker1 = layout.findViewById(R.id.numPicker1);
+        numPicker2 = layout.findViewById(R.id.numPicker2);
+        numPicker3 = layout.findViewById(R.id.numPicker3);
+        numPicker4 = layout.findViewById(R.id.numPicker4);
+        numPicker5 = layout.findViewById(R.id.numPicker5);
+
+        numPicker0.setMaxValue(9);
+        numPicker0.setMinValue(0);
+
+        numPicker1.setMaxValue(9);
+        numPicker1.setMinValue(0);
+
+        numPicker2.setMaxValue(9);
+        numPicker2.setMinValue(0);
+
+        numPicker3.setMaxValue(9);
+        numPicker3.setMinValue(0);
+
+        numPicker4.setMaxValue(9);
+        numPicker4.setMinValue(0);
+
+        numPicker5.setMaxValue(9);
+        numPicker5.setMinValue(0);
+
+        new AlertDialog.Builder(this)
+                .setTitle("title")
+                .setView(layout)
+                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        // OK button pressed
+                        num[0] = numPicker0.getValue();
+                        num[1] = numPicker1.getValue();
+                        num[2] = numPicker2.getValue();
+                        num[3] = numPicker3.getValue();
+                        num[4] = numPicker4.getValue();
+                        num[5] = numPicker5.getValue();
+
+                        int result = num[0]*10000 + num[1]*1000 + num[2]*100 + num[3]*10 + num[4];
+                        drawCircleView.addValue(result);
+                    }
+                })
+                .setNegativeButton("Cancel", null)
+                .show();
     }
 }
